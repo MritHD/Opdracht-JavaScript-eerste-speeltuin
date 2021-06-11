0f97500d1d3e5016b1dfb15d1bccca5731610275
MritHD <84324249+MritHD@users.noreply.github.com>
2021-06-11 20:30:40 +0200

    Add files via upload

---
+++ Opdracht 9 - JavaScript/scripts.js
@@ -0,0 +1,28 @@
+//opdracht 4
+console.log("Hello Winc Academy");
+
+//opdracht 5.
+let name = 'Marit';
+console.log(name); 
+
+//opdracht 7.
+console.log (4+4);
+console.log("4"+"4");
+
+//opdracht 8.
+console.log(8*2);
+console.log(9-5);
+console.log(7*3);
+console.log(6/2);
+console.log(25%100);
+
+//opdracht 9.
+let age = 'drieënveertig';
+
+
+
+
+
+
+
+
